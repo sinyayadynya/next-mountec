1a52001019890620cd950ec6ceb2478b2592452d
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-24 02:18:32 +0600

    Update [...slug].tsx

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,86 +1,86 @@
-import * as React from 'react';
-import { GetStaticPathsResult, GetStaticPropsResult } from 'next';
-import Head from 'next/head';
-import { DrupalNode } from 'next-drupal';
+import * as React from "react"
+import { GetStaticPathsResult, GetStaticPropsResult } from "next"
+import Head from "next/head"
+import { DrupalNode } from "next-drupal"
 
-import { drupal } from 'lib/drupal';
+import { drupal } from "lib/drupal"
 
-import { Layout } from 'components/layout';
-import { Metatag } from 'components/metatag';
-import { Node, RESOURCE_TYPES, RESOURCE_INCLUDES } from 'components/node';
+import { Layout } from "components/layout"
+import { Metatag } from "components/metatag"
+import { Node, RESOURCE_TYPES, RESOURCE_INCLUDES } from "components/node"
 
 interface NodePageProps {
-    resource: DrupalNode;
+  resource: DrupalNode
 }
 
 export default function NodePage({ resource }: NodePageProps) {
-    if (!resource) return null;
+  if (!resource) return null
 
-    return (
-        <Layout>
-            <Head>
-                <title>{resource.title}</title>
-                {resource.metatag && <Metatag data={resource.metatag} />}
-            </Head>
-            <Node resource={resource} />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>{resource.title}</title>
+        {resource.metatag && <Metatag data={resource.metatag} />}
+      </Head>
+      <Node resource={resource} />
+    </Layout>
+  )
 }
 
 export async function getStaticPaths(context): Promise<GetStaticPathsResult> {
-    return {
-        paths: await drupal.getStaticPathsFromContext(RESOURCE_TYPES, context),
-        fallback: 'blocking',
-    };
+  return {
+    paths: await drupal.getStaticPathsFromContext(RESOURCE_TYPES, context),
+    fallback: "blocking",
+  }
 }
 
 export async function getStaticProps(
-    context
+  context
 ): Promise<GetStaticPropsResult<NodePageProps>> {
-    const path = await drupal.translatePathFromContext(context);
+  const path = await drupal.translatePathFromContext(context)
 
-    if (!path) {
-        return {
-            notFound: true,
-        };
-    }
-
-    const type = path.jsonapi.resourceName;
-
-    let params = {};
-    if (RESOURCE_INCLUDES[type]) {
-        params = {
-            include: RESOURCE_INCLUDES[type],
-        };
-    }
-
-    const resource = await drupal.getResourceFromContext<DrupalNode>(
-        path,
-        context,
-        {
-            params,
-        }
-    );
-
-    // At this point, we know the path exists and it points to a resource.
-    // If we receive an error, it means something went wrong on Drupal.
-    // We throw an error to tell revalidation to skip this for now.
-    // Revalidation can try again on next request.
-    if (!resource) {
-        throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`);
-    }
-
-    // If we're not in preview mode and the resource is not published,
-    // Return page not found.
-    if (!context.preview && resource?.status === false) {
-        return {
-            notFound: true,
-        };
-    }
-
+  if (!path) {
     return {
-        props: {
-            resource,
-        },
-    };
+      notFound: true,
+    }
+  }
+
+  const type = path.jsonapi.resourceName
+
+  let params = {}
+  if (RESOURCE_INCLUDES[type]) {
+    params = {
+      include: RESOURCE_INCLUDES[type],
+    }
+  }
+
+  const resource = await drupal.getResourceFromContext<DrupalNode>(
+    path,
+    context,
+    {
+      params,
+    }
+  )
+
+  // At this point, we know the path exists and it points to a resource.
+  // If we receive an error, it means something went wrong on Drupal.
+  // We throw an error to tell revalidation to skip this for now.
+  // Revalidation can try again on next request.
+  if (!resource) {
+    throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`)
+  }
+
+  // If we're not in preview mode and the resource is not published,
+  // Return page not found.
+  if (!context.preview && resource?.status === false) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      resource,
+    },
+  }
 }
