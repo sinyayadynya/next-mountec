bb1f88ffad63fe8caf3c5ad944f1fd903b18991b
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-15 01:05:16 +0600

    Update

---
+++ next-i18next.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const config = require('./site.config');
+
+module.exports = {
+    i18n: {
+        defaultLocale: config.defaultLocale,
+        locales: Object.keys(config.locales),
+        localePath: path.resolve('./public/locales'),
+    },
+    ns: ['common'],
+};
---
+++ site.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'Mountec Corp',
+    slogan: 'Mountec',
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    drupalBaseUrl: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+    defaultLocale: 'en',
+    locales: {
+        en: 'English',
+        fr: 'French',
+        // ru: 'Русский',
+    },
+};
