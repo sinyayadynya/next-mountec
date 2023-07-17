1194749ec92d108bd91ed9a332652b558b4bc73a
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-18 00:05:45 +0600

    Update work

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,14 +1,67 @@
 import Head from "next/head"
+import Image from 'next/image'
 import { GetStaticPropsResult } from "next"
 import { DrupalNode } from "next-drupal"
 
 import { drupal } from "lib/drupal"
 import { RootLayout } from "components/RootLayout"
+import { Border } from 'components/Border'
 import { Container } from 'components/Container'
 import { ContactSection } from 'components/ContactSection'
-import { FadeIn } from 'components/FadeIn'
+import { FadeIn, FadeInStagger } from 'components/FadeIn'
 import { PageIntro } from 'components/PageIntro'
+import { Testimonial } from 'components/Testimonial'
 import { NodeCaseStudyTeaser } from "components/node--case_study--teaser"
+
+import logoBrightPath from 'images/clients/bright-path/logo-dark.svg'
+import logoFamilyFund from 'images/clients/family-fund/logo-dark.svg'
+import logoGreenLife from 'images/clients/green-life/logo-dark.svg'
+import logoHomeWork from 'images/clients/home-work/logo-dark.svg'
+import logoMailSmirk from 'images/clients/mail-smirk/logo-dark.svg'
+import logoNorthAdventures from 'images/clients/north-adventures/logo-dark.svg'
+import logoPhobia from 'images/clients/phobia/logo-dark.svg'
+import logoUnseal from 'images/clients/unseal/logo-dark.svg'
+
+// To make dynamic
+const clients = [
+    ['Phobia', logoPhobia],
+    ['Family Fund', logoFamilyFund],
+    ['Unseal', logoUnseal],
+    ['Mail Smirk', logoMailSmirk],
+    ['Home Work', logoHomeWork],
+    ['Green Life', logoGreenLife],
+    ['Bright Path', logoBrightPath],
+    ['North Adventures', logoNorthAdventures],
+  ]
+
+  function Clients() {
+    return (
+      <Container className="mt-24 sm:mt-32 lg:mt-40">
+        <FadeIn>
+          <h2 className="font-display text-2xl font-semibold text-neutral-950">
+            You’re in good company
+          </h2>
+        </FadeIn>
+        <FadeInStagger className="mt-10" faster>
+          <Border as={FadeIn} />
+          <ul
+            role="list"
+            className="grid grid-cols-2 gap-x-8 gap-y-12 sm:grid-cols-3 lg:grid-cols-4"
+          >
+            {clients.map(([client, logo]) => (
+              <li key={client} className="group">
+                <FadeIn className="overflow-hidden">
+                  <Border className="pt-12 group-[&:nth-child(-n+2)]:-mt-px sm:group-[&:nth-child(3)]:-mt-px lg:group-[&:nth-child(4)]:-mt-px">
+                    <Image src={logo} alt={client} unoptimized />
+                  </Border>
+                </FadeIn>
+              </li>
+            ))}
+          </ul>
+        </FadeInStagger>
+      </Container>
+    )
+  }
 
 
 interface IndexPageProps {
@@ -52,14 +105,27 @@ export default function IndexPage({ nodes }: IndexPageProps) {
                 )}
 
             </div>
-            <ContactSection />
 
         </Container>
 
       </div>
+
+        <Testimonial
+            className="mt-24 sm:mt-32 lg:mt-40"
+            client={{ name: 'Mail Smirk', logo: logoMailSmirk }}
+        >
+            We approached <em>Studio</em> because we loved their past work. They
+            delivered something remarkably similar in record time.
+        </Testimonial>
+
+        <Clients />
+
+        <ContactSection />
+
     </RootLayout>
   )
 }
+
 
 export async function getStaticProps(
   context
