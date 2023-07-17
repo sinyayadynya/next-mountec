3da0a8461b26d0d01df62b968ec1a76260d1edc9
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-17 07:27:29 +0600

    Update node--article--teaser.tsx

--- components/node--article--teaser.tsx
+++ components/node--article--teaser.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { DrupalNode } from 'next-drupal';
@@ -16,15 +17,25 @@ interface NodeArticleTeaserProps {
 }
 
 export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
+    const [imageUrl, setImageUrl] = useState(null);
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/jsonapi/file/file/${node.uid.image.id}`)
+            .then(response => response.json())
+            .then(data => {
+                // The image URL should be available under data.data.attributes.url
+                const imageUrl = `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${data.data.attributes.uri.url}`;
+                setImageUrl(imageUrl);
+        });
+    }, [node.uid.image.id]);
+
     return (
         <FadeIn key={node.id}>
             <article {...props}>
-
                 <Border className="pt-16">
                   <div className="relative lg:-mx-4 lg:flex lg:justify-end">
                     <div className="pt-10 lg:w-2/3 lg:flex-none lg:px-4 lg:pt-0">
                       <h2 className="font-display text-2xl font-semibold text-neutral-950">
-
                             <Link
                                 href={node.path.alias}
                                 passHref
@@ -32,8 +43,6 @@ export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
                                 {node.title}
                             </Link>
                         </h2>
-
-
                         <dl className="lg:absolute lg:left-0 lg:top-0 lg:w-1/3 lg:px-4">
                             <dt className="sr-only">Published</dt>
                             <dd className="absolute left-0 top-0 text-sm text-neutral-950 lg:static">
@@ -45,34 +54,34 @@ export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
                                 <>
                                     <dt className="sr-only">Author</dt>
                                     <dd className="mt-6 flex gap-x-4">
-                                        {/* <div className="flex-none overflow-hidden rounded-xl bg-neutral-100">
-                                            <Image
-                                                alt=""
-                                                {...article.author.image}
-                                                className="h-12 w-12 object-cover grayscale"
-                                            />
-                                            {node.author.image}
-                                        </div> */}
+                                        <div className="flex-none overflow-hidden rounded-xl bg-neutral-100">
+                                            {imageUrl &&
+                                                <Image
+                                                    alt={node.uid.display_name}
+                                                    src={imageUrl}
+                                                    className="h-12 w-12 object-cover grayscale"
+                                                    width={48}
+                                                    height={48}
+                                                />
+                                            }
+                                        </div>
                                         <div className="text-sm text-neutral-950">
                                             <div className="font-semibold">
                                                 {node.uid?.display_name}
                                             </div>
-                                            {/* <div>
-                                                {article.author.role}
-                                            </div> */}
+                                            <div>
+                                                {node.uid?.job_title}
+                                            </div>
                                         </div>
                                     </dd>
                                 </>
                             ) : null}
-
                         </dl>
-
-                            {node.description?.processed && (
-                                <div className="mt-6 max-w-2xl text-base text-neutral-600">
-                                    <FormattedText processed={node.description.processed} />
-                                </div>
-                            )}
-
+                        {node.description?.processed && (
+                            <div className="mt-6 max-w-2xl text-base text-neutral-600">
+                                <FormattedText processed={node.description.processed} />
+                            </div>
+                        )}
                         <Button
                             href={node.path.alias}
                             aria-label={`Read more: ${node.title}`}
@@ -80,13 +89,9 @@ export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
                         >
                             Read more
                         </Button>
-
                     </div>
                 </div>
             </Border>
-
-
-
             </article>
         </FadeIn>
     );
