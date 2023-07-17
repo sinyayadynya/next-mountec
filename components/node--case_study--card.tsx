bf0f726c609eeb8c7745e805f9675131fcd1392f
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-17 18:15:43 +0600

    Create node--case_study--card.tsx

---
+++ components/node--case_study--card.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { DrupalNode } from 'next-drupal';
+import { FormattedText } from "components/formatted-text";
+
+import { absoluteUrl } from 'lib/absolute-url';
+import { formatDate } from 'lib/format-date';
+
+import { Border } from 'components/Border'
+import { Button } from 'components/Button'
+import { FadeIn } from 'components/FadeIn'
+
+
+interface NodeCaseStudyCardProps {
+    node: DrupalNode;
+}
+
+export function NodeCaseStudyCard({ node, ...props }: NodeCaseStudyCardProps) {
+    return (
+        <FadeIn key={node.id} className="flex">
+            <article {...props} className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+
+                <h3>
+                  <Link href={node.path.alias}>
+                        <span className="absolute inset-0 rounded-3xl" />
+                        {/* <Image
+                            src={node.logo}
+                            alt={node.client}
+                            className="h-16 w-16"
+                            unoptimized
+                        /> */}
+                    </Link>
+                </h3>
+                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
+                  <time dateTime={node.copyright_year} className="font-semibold">
+                    {node.copyright_year}
+                  </time>
+                  <span className="text-neutral-300" aria-hidden="true">
+                    /
+                  </span>
+                  <span>Case study</span>
+                </p>
+                <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
+                  {node.title}
+                </p>
+
+                {node.description?.processed && (
+                    <div className="mt-4 text-base text-neutral-600">
+                        <FormattedText processed={node.description.processed} />
+                    </div>
+                )}
+
+            </article>
+        </FadeIn>
+    );
+}
