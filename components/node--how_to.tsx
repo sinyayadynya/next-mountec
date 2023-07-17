9d311ac2b8d03c05e882d1d3eaa102628d1095a0
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-17 19:30:14 +0600

    Update node--how_to.tsx

--- components/node--how_to.tsx
+++ components/node--how_to.tsx
@@ -1,6 +1,8 @@
 import { DrupalNode } from "next-drupal";
 import { FormattedText } from "components/formatted-text";
 import { DrupalEntity } from "components/entity";
+import { DrupalJsonApiParams } from "drupal-jsonapi-params";
+
 
 import { ContactSection } from 'components/ContactSection'
 import { Container } from 'components/Container'
@@ -8,7 +10,11 @@ import { FadeIn } from 'components/FadeIn'
 import { PageIntro } from 'components/PageIntro'
 import { SectionIntro } from 'components/SectionIntro'
 import { StylizedImage } from 'components/StylizedImage'
+import { useEffect, useState } from "react";
 
+const params = new DrupalJsonApiParams();
+
+params.addInclude(['step']);
 
 interface NodeHowToProps {
   node: DrupalNode;
@@ -44,21 +50,33 @@ function Section({ title, image, children }) {
     )
   }
 
-function StepType1({ data }) {
+  function StepType1({ data }) {
+    const [imageUrl, setImageUrl] = useState(null);
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/jsonapi/media/image_object/${data.image.id}`)
+            .then(response => response.json())
+            .then(data => {
+                const imageUrl = `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${data.data.attributes.uri.url}`;
+                setImageUrl(imageUrl);
+            });
+    }, [data.image.id]);
+
     return (
-        <Section title={data.name} image={{ src: data.image }}>
-        <div className="space-y-6 text-base text-neutral-600">
-          {/* <FormattedText processed={data.description.processed} /> */}
-        </div>
-      </Section>
+        <Section title={data.name} image={{ src: imageUrl }}>
+            <div className="space-y-6 text-base text-neutral-600">
+                <FormattedText processed={data.description.processed} />
+            </div>
+        </Section>
     )
-  }
+}
+
 
   function StepType2({ data }) {
     return (
         <Section title={data.name} image={{ src: data.image }}>
         <div className="space-y-6 text-base text-neutral-600">
-          {/* <FormattedText processed={data.description.processed} /> */}
+          <FormattedText processed={data.description.processed} />
         </div>
       </Section>
     )
@@ -68,12 +86,11 @@ function StepType1({ data }) {
     return (
         <Section title={data.name} image={{ src: data.image }}>
         <div className="space-y-6 text-base text-neutral-600">
-          {/* <FormattedText processed={data.description.processed} /> */}
+          <FormattedText processed={data.description.processed} />
         </div>
       </Section>
     )
   }
-
 
   export function NodeHowTo({ node, ...props }: NodeHowToProps) {
     return (
@@ -92,35 +109,25 @@ function StepType1({ data }) {
                     )}
                 </PageIntro>
 
-                {node.step && node.step.map((step, index) => {
-                switch (index) {
-                    case 0:
-                    return <StepType1 key={index} data={step} />
-                    // Add other step types here
-                    default:
-                    return null;
-                }
-                })}
+                <div className="mt-24 space-y-24 [counter-reset:section] sm:mt-32 sm:space-y-32 lg:mt-40 lg:space-y-40">
+
 
                 {node.step && node.step.map((step, index) => {
-                switch (index) {
-                    case 1:
-                    return <StepType2 key={index} data={step} />
-                    // Add other step types here
-                    default:
-                    return null;
-                }
+                    switch (index) {
+                        case 0:
+                        return <StepType1 key={index} data={step} />
+                        case 1:
+                        return <StepType2 key={index} data={step} />
+                        case 2:
+                        return <StepType3 key={index} data={step} />
+                        default:
+                        return null;
+                    }
                 })}
 
-                {node.step && node.step.map((step, index) => {
-                switch (index) {
-                    case 2:
-                    return <StepType3 key={index} data={step} />
-                    // Add other step types here
-                    default:
-                    return null;
-                }
-                })}
+                </div>
+
+
             </article>
 
             <ContactSection />
