ec1462a6597596ab7fa8119d210df377f3dc9865
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-08-01 12:31:13 +0600

    Update node--how_to.tsx

--- components/node--how_to.tsx
+++ components/node--how_to.tsx
@@ -1,7 +1,12 @@
-import { DrupalNode } from "next-drupal";
+// ./components/node--how_to.tsx
+
+import { DrupalClient, DrupalNode } from "next-drupal";
 import { FormattedText } from "components/formatted-text";
 import { DrupalEntity } from "components/entity";
 import { DrupalJsonApiParams } from "drupal-jsonapi-params";
+import { drupal } from "lib/drupal"
+import { getResourceCollection } from 'next-drupal';
+import HowToStepSerializer from 'lib/serializers/HowToStepSerializer';
 
 
 import { ContactSection } from 'components/ContactSection'
@@ -30,8 +35,31 @@ async function fetchImage(id) {
 }
 
 interface NodeHowToProps {
-  node: DrupalNode;
+    node: DrupalNode;
 }
+
+
+const client = new DrupalClient(process.env.NEXT_PUBLIC_DRUPAL_BASE_URL);
+
+
+export async function getStaticProps() {
+    const howToData = await client.getResourceCollection("/jsonapi/node/how_to")
+
+    return {
+      props: {
+        howToData,
+      },
+      revalidate: 60 // Set revalidation time as needed
+    }
+}
+
+interface NodeHowToProps {
+    node: DrupalNode;
+    howToData: any; // You can replace 'any' with the specific type if known
+  }
+
+
+
 
 function Section({ title, image, children }) {
     return (
@@ -66,7 +94,7 @@ function Section({ title, image, children }) {
 function StepType1({ data }) {
     return (
         <Section title={data.name} image={{ src: processStep1 }}>
-            <div className="space-y-6 text-base text-neutral-600 [&>ul]:mt-4 [&>ul]:flex [&>ul]:flex-wrap [&>ul]:gap-4 [&>ul>li]:rounded-full [&>ul>li]:bg-neutral-100 [&>ul>li]:px-4 [&>ul>li]:py-1.5 [&>ul>li]:text-base [&>ul>li]:text-neutral-600 [&>h3]:mt-12 [&>h3]:font-display [&>h3]:text-base [&>h3]:font-semibold [&>h3]:text-neutral-950">
+            <div className="space-y-6 text-base text-neutral-600 [&>ul]:mt-4 [&>ul]:flex [&>ul]:flex-wrap [&>ul>:gap-4 [&>ul>li]:rounded-full [&>ul>li]:bg-neutral-100 [&>ul>li]:px-4 [&>ul>li]:py-1.5 [&>ul>li]:text-base [&>ul>li]:text-neutral-600 [&>h3]:mt-12 [&>h3]:font-display [&>h3]:text-base [&>h3]:font-semibold [&>h3]:text-neutral-950">
                 <FormattedText processed={data.description.processed} />
             </div>
         </Section>
@@ -94,6 +122,20 @@ function StepType3({ data }) {
 }
 
 function Values() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      fetch(`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/api/values`)
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    // If data is null, render nothing
+    if (!data) {
+      return null;
+    }
+
+    // Render the block data
     return (
       <div className="relative mt-24 pt-24 sm:mt-32 sm:pt-32 lg:mt-40 lg:pt-40">
         <div className="absolute inset-x-0 top-0 -z-10 h-[884px] overflow-hidden rounded-t-4xl bg-gradient-to-b from-neutral-50">
@@ -104,51 +146,31 @@ function Values() {
         </div>
 
         <SectionIntro
-          eyebrow="Our values"
-          title="Balancing reliability and innovation"
+          eyebrow={data[0].title}
+          title={data[0].headline}
         >
           <p>
-            We strive to stay at the forefront of emerging trends and
-            technologies, while completely ignoring them and forking that old
-            Rails project we feel comfortable using. We stand by our core values
-            to justify that decision.
+            {data[0].description}
           </p>
         </SectionIntro>
 
         <Container className="mt-24">
           <GridList>
-            <GridListItem title="Meticulous">
-              The first part of any partnership is getting our designer to put
-              your logo in our template. The second step is getting them to do the
-              colors.
-            </GridListItem>
-            <GridListItem title="Efficient">
-              We pride ourselves on never missing a deadline which is easy because
-              most of the work was done years ago.
-            </GridListItem>
-            <GridListItem title="Adaptable">
-              Every business has unique needs and our greatest challenge is
-              shoe-horning those needs into something we already built.
-            </GridListItem>
-            <GridListItem title="Honest">
-              We are transparent about all of our processes, banking on the simple
-              fact our clients never actually read anything.
-            </GridListItem>
-            <GridListItem title="Loyal">
-              We foster long-term relationships with our clients that go beyond
-              just delivering a product, allowing us to invoice them for decades.
-            </GridListItem>
-            <GridListItem title="Innovative">
-              The technological landscape is always evolving and so are we. We are
-              constantly on the lookout for new open source projects to clone.
-            </GridListItem>
+            {data.map((item, index) => (
+              <GridListItem key={index} title={item.item_name}>
+                {item.item_description}
+              </GridListItem>
+            ))}
           </GridList>
         </Container>
       </div>
-    )
+    );
 }
 
-export function NodeHowTo({ node, ...props }: NodeHowToProps) {
+
+
+
+export function NodeHowTo({ node, howToData, ...props }: NodeHowToProps) {
     return (
         <>
             <article {...props}>
@@ -160,7 +182,7 @@ export function NodeHowTo({ node, ...props }: NodeHowToProps) {
                     )}
                     {node.text?.processed && (
                         <div className="mt-10 max-w-2xl space-y-6 text-base">
-                            <FormattedText processed={node.text.processed} />
+                        <FormattedText processed={node.text.processed} />
                         </div>
                     )}
                 </PageIntro>
